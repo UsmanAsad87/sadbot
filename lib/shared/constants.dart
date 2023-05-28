b58dd94d0b099575d48f76d641230dbf764d1317
adnanijaz92 <adnan_ijaz@outlook.com>
2023-05-28 19:22:14 +0500

    all code

---
+++ lib/shared/constants.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class Constants {
+  static String appId = "1:1051373939903:web:150c63ea4d93c429c3aa0a";
+  static String apiKey = "AIzaSyDSMkaySQ0YFHhB4WDyWDuEX8Cd6odR_7s";
+  static String messagingSenderId = "1051373939903";
+  static String projectId = "groupchatapp-69370";
+  //final primaryColor = const Color(0xFFee7b64);
+  final primaryColor = const Color(0xFF2F2B53);
+}
